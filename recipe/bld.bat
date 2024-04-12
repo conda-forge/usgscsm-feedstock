5874e33d91c8543930e9e609e5d00fb498814426
Kelvin <krodriguez@usgs.gov>
2024-04-12 07:54:08 -0700

    strings? maybe?

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,6 @@
 mkdir build
 cd build
+
 cmake -G "Visual Studio 15 2017 Win64" ^
     -D CMAKE_BUILD_TYPE=Release ^
     -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
@@ -8,7 +9,8 @@ cmake -G "Visual Studio 15 2017 Win64" ^
     -D USGSCSM_BUILD_DOCS=OFF  ^
     -D ALE_BUILD_DOCS=OFF  ^
     -D USGSCSM_EXTERNAL_DEPS=OFF ^ 
-     %SRC_DIR%
+     "%SRC_DIR%"
+
 if errorlevel 1 exit 1
 
 cmake --build . --target ALL_BUILD --config Release
