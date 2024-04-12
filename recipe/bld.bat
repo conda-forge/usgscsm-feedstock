ffd694410d427f7bfae1c1476df5adaeb3d6adb1
Kelvin <krodriguez@usgs.gov>
2024-04-12 08:35:14 -0700

    switching to make

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 
-cmake -G "Make" ^
+cmake -G "NMake Makefiles" ^
     -D CMAKE_BUILD_TYPE=Release ^
     -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -D CMAKE_PREFIX_PATH=%PREFIX% ^
